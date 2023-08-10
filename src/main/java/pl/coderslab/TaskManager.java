8fea4df730af207bc104b5d580dfec3d90449f72
ayazvinski <a.yazvinski@limitless.pl>
2023-08-10 20:02:55 +0200

    metoda selectOption

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -4,5 +4,18 @@ public class TaskManager {
 
     public static void main(String[] args) {
 
+        selectOption(args);
     }
+
+    public static void selectOption(String[] args) {
+        String[] options = new String[] {"add","remove", "list","exit"};
+
+        System.out.println(ConsoleColors.BLUE + "Please select an option");
+
+        for (int i = 0; i < options.length; i++) {
+            System.out.println(ConsoleColors.RESET + options[i]);
+        }
+    }
+
+
 }
